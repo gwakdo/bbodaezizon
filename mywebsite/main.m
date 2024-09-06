95df2f973ec829b9510dea5e4a7d927f8ca38765
doyun <dob2s@naver.com>
2024-09-06 20:49:25 +0900

    Initial Commit

---
+++ mywebsite/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mywebsite
+//
+//  Created by doyun on 2024/09/06.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
